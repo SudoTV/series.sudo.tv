316700aededcbcc24f9b3b38a3f80066433b6098
WMXPY <wm@mengw.io>
2021-12-23 20:13:19 -0600

    use new language switch pattern

--- assets/js/language-common.js
+++ assets/js/language-common.js
@@ -1,3 +1,8 @@
+const availableLanguages = [
+    'zh-CN',
+    'en-US',
+];
+
 const getLanguage = () => {
 
     const localStorageLanguage = localStorage.getItem('sudo-tv-language');
@@ -9,4 +14,17 @@ const getLanguage = () => {
     localStorage.setItem('sudo-tv-language', language);
 
     return language;
+};
+
+const setLanguage = (language) => {
+
+    localStorage.setItem('sudo-tv-language', language);
+
+    const path = window.location.pathname;
+
+    if (availableLanguages.some((each) => {
+        return path.startsWith(`/${each}`);
+    })) {
+        window.location.href = `/${language}${path.substring(6)}`;
+    }
 };
